da3872e10dfd567b06f7ffc0f50dc241a6d9a651
Dawson <dmbezehertny@gmail.com>
2024-01-17 07:30:53 -0500

    update comments

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 'use strict';
-/** Database setup for jobly. */
+/** Database setup for Only Locks */
 const { Client } = require('pg');
 const dotenv = require('dotenv');
 const { getDatabaseUri } = require('./config');
